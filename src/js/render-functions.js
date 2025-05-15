1d8f4ba20eca5d9ca4c2b568facfb65475649cd1
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-05-15 11:31:01 +0300

    js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,7 +7,7 @@ const loader = document.querySelector('.loader-container');
 let lightbox = new SimpleLightbox('.gallery a');
 
 export function createGallery(images) {
-  const markup = image
+  const markup = images
     .map(
       ({
         webformatURL,
