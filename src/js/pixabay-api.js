57f3f4a7021485d2d0e830c8fc38c9ab8d05e770
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-05-15 11:10:13 +0300

    js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,29 @@
 import axios from 'axios';
-
-const API_KEY = '50282426-bcbed3422443d463b1ec5299f';
-const BASE_URL = 'https://pixabay.com/api/';
-
-export async function getImagesByQuery(query) {
-    const params = {
-        key: API_KEY,
+export default function getImagesByQuery(query) {
+    const MAIN_URL = 'https://pixabay.com';
+    const END_POINT = '/api/';
+    const params = new URLSearchParams({
+        key: '50282426-bcbed3422443d463b1ec5299f',
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+    });
+    const url = `${MAIN_URL}${END_POINT}?${params}`;
+    const headers = {
+        'X-RateLimit-Limit':
+            'The maximum number of requests that the consumer is permitted to make in 60 seconds.',
+        'X-RateLimit-Remaining':
+            'The number of requests remaining in the current rate limit window.',
+        'X-RateLimit-Reset':
+            'The remaining time in seconds after which the current rate limit window resets.',
     };
-
-    const response = await axios.get(BASE_URL, { params });
-    return response.data;
+    return axios
+        .get(url)
+        .then(response => {
+            return response.data.hits;
+        })
+        .catch(error => {
+            console.log('Помилка при запиті до Pixabay:', error);
+        });
 }
